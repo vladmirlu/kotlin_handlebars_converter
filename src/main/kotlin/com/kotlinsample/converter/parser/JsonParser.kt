638112a179b9a06ea0b7d787d65c6654a8ce5518
Volodydyr <lubikvova@gmail.com>
2020-03-03 03:30:02 +0200

    Add RandomAccessFile

--- src/main/kotlin/com/kotlinsample/converter/parser/JsonParser.kt
+++ src/main/kotlin/com/kotlinsample/converter/parser/JsonParser.kt
@@ -6,12 +6,19 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.jknack.handlebars.Template
 import com.google.gson.Gson
 import com.kotlinsample.converter.model.calendar.CalendarInfo
+import com.kotlinsample.converter.model.security.Security
+import com.kotlinsample.converter.writer.FileService
 import com.kotlinsample.converter.writer.HandlebarsProvider
+import org.json.simple.JSONArray
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.io.File
+import java.io.FileReader
 import java.io.IOException
+import java.io.Reader
 import java.nio.file.Files
 import java.nio.file.Paths
 import javax.annotation.PostConstruct
@@ -23,14 +30,8 @@ class JsonParser {
     @Value("\${input.data.folder}")
     lateinit var inputDataFolder: String
 
-    @Value("\${output.data.folder}")
-    lateinit var outputDataFolder: String
-
-    @Value("\${output.data.calendar}")
-    lateinit var outputDataCalendar: String
-
-    @Value("\${output.data.security}")
-    lateinit var outputDataSecurity: String
+    @Autowired
+    lateinit var fileService: FileService
 
     @Autowired
     lateinit var handlebarsProvider: HandlebarsProvider
@@ -44,33 +45,26 @@ class JsonParser {
         val jsonMapper = ObjectMapper()
         var calendarInfos: List<CalendarInfo> = jsonMapper.readValue(jsonFile, object : TypeReference<List<CalendarInfo>>() {})
 
-        var outputDir:File = File(outputDataFolder)
-        if(!outputDir.exists()){
-            Files.createDirectory(Paths.get(outputDataFolder));
-        }
-        var outputFile:File = File(outputDir.absolutePath + outputDataCalendar)
-        if(!outputFile.exists()){
-            outputFile.createNewFile()
-        }
-        println(outputFile.absolutePath)
-        /*calendarInfos.filter {cal -> !cal.marketDay}.forEach { c ->
+
+        calendarInfos.filter {cal -> !cal.marketDay}.forEach { c ->
             var calInfoJsonStr = gson.toJson(c)
-            //println(calInfo)
             val jsonNode: JsonNode = ObjectMapper().readValue(calInfoJsonStr, JsonNode::class.java)
             val template: Template = handlebarsProvider.getTemplate("CalendarInfo.txt")
-            println(template.apply(handlebarsProvider.getContext(jsonNode)))
-        }*/
+
+            var handlebarsStr = template.apply(handlebarsProvider.getContext(jsonNode))
+            fileService.writeIntoFile(handlebarsStr.toByteArray())
+        }
     }
 
-    /*@PostConstruct
+    @PostConstruct
     @Throws(IOException::class)
     fun parseSecurities(){
-        var jsonFile = File(jsonDataFolder + "securities.json")
+        var jsonFile = File(inputDataFolder + "securities.json")
         var reader: Reader = FileReader(jsonFile)
         val parser = JSONParser()
         var jsonObject: JSONObject = parser.parse(reader) as JSONObject
         var securitiesJson = (jsonObject.get("securities")) as JSONArray
         securitiesJson.forEach{value -> println(gson.fromJson(value.toString(), Security::class.java)) }
-    }*/
+    }
 
 }
